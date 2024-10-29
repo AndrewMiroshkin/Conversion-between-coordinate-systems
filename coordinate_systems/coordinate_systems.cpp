e286d7234253bf1fd6564e6b1db8906ea476c777
Andrew Miroshkin <miroskinandrij12@gmail.com>
2024-10-29 19:42:33 +0200

    v2.0

--- coordinate_systems/coordinate_systems.cpp
+++ coordinate_systems/coordinate_systems.cpp
@@ -5,6 +5,7 @@
 #define PI 3.14159265358979323846
 
 class Cartesian; 
+class Spherical;
 class Polar;
 
 class Polar {
@@ -23,8 +24,30 @@ public:
 		return (fabs(r - other.r) < 1e-6) && (fabs(theta - other.theta) < 1e-6);
 	}
 
-	void print() const {
+	void print() {
 		std::cout << "Полярна система координат: (r: " << r << ", theta: " << theta << ")\n";
+	}
+};
+
+class Spherical {
+private:
+	double r; 
+	double theta; 
+	double phi; 
+
+public:
+	Spherical(double radius, double azimuth, double polar) : r(radius), theta(azimuth), phi(polar) {}
+
+	friend Cartesian sphericalToCartesian(const Spherical&);
+
+	friend Spherical cartesianToSpherical(const Cartesian&);
+
+	bool isEqual(const Spherical& other) const {
+		return (fabs(r - other.r) < 1e-6) && (fabs(theta - other.theta) < 1e-6) && (fabs(phi - other.phi) < 1e-6);
+	}
+
+	void print() {
+		std::cout << "Сферична система координат: (r: " << r << ", theta: " << theta << ", phi: " << phi << ")\n";
 	}
 };
 
@@ -32,16 +55,29 @@ class Cartesian {
 private:
 	double x;
 	double y;
+	double z;
 
 public:
-	Cartesian(double x, double y) : x(x), y(y) {}
+	Cartesian() : x(0), y(0), z(0) {}
+
+	Cartesian(double x, double y) : x(x), y(y), z(0) {}
+
+	Cartesian(double x, double y, double z) : x(x), y(y), z(z) {}
 
 	friend Cartesian polarToCartesian(const Polar&);
 
 	friend Polar cartesianToPolar(const Cartesian&);
 
-	void print() const {
+	friend Cartesian sphericalToCartesian(const Spherical&);
+
+	friend Spherical cartesianToSpherical(const Cartesian&);
+
+	void print2D() {
 		std::cout << "Декартова система координат: (x: " << x << ", y: " << y << ")\n";
+	}
+
+	void print3D() {
+		std::cout << "Декартова система координат: (x: " << x << ", y: " << y << ", z: " << z << ")\n";
 	}
 };
 
@@ -50,7 +86,20 @@ Cartesian polarToCartesian(const Polar& polar) {
 }
 
 Polar cartesianToPolar(const Cartesian& cartesian) {
-	return Polar(sqrt(cartesian.x * cartesian.x + cartesian.y * cartesian.y), atan2(cartesian.y, cartesian.x));
+	return Polar(sqrt(pow(cartesian.x, 2) + pow(cartesian.y, 2)), atan2(cartesian.y, cartesian.x));
+}
+
+Cartesian sphericalToCartesian(const Spherical& spherical) {
+	return Cartesian(
+		spherical.r * sin(spherical.phi) * cos(spherical.theta),
+		spherical.r * sin(spherical.phi) * sin(spherical.theta),
+		spherical.r * cos(spherical.phi)
+	);
+}
+
+Spherical cartesianToSpherical(const Cartesian& cartesian) {
+	double r = sqrt(pow(cartesian.x, 2) + pow(cartesian.y, 2) + pow(cartesian.z, 2));
+	return Spherical(r, atan2(cartesian.y, cartesian.x), acos(cartesian.z / r));
 }
 
 int main() {
@@ -59,37 +108,75 @@ int main() {
 	system("cls");
 
 	bool correct = true;
-	std::vector<Cartesian> cartesianPoints; 
-	std::vector<Polar> convertedPolarPoints; 
+	std::vector<Cartesian> cartesianPoints2D; 
+	std::vector<Polar> convertedPolarPoints;
 	std::vector<Polar> polarPoints = {
-		{5, 0},
+		{0, 0},
 		{5, PI / 6},
 		{3, PI / 4},
-		{7, PI / 3}
+		{8, PI / 3}
 	}; 
+
+	std::vector<Cartesian> cartesianPoints3D;
+	std::vector<Spherical> convertedSphericalPoints;
+	std::vector<Spherical> sphericalPoints = {
+		{1, 1, 1},
+		{5, PI / 6, PI / 4},
+		{3, PI / 4, PI / 3},
+		{8, PI / 3, PI / 6}
+	};
 	
-	for (const auto& polar : polarPoints) {
-		cartesianPoints.push_back(polarToCartesian(polar));
-	}
-
-	for (const auto& cartesian : cartesianPoints) {
-		convertedPolarPoints.push_back(cartesianToPolar(cartesian));
-	}
-
-	for (auto n : convertedPolarPoints) n.print();
-	std::cout << '\n';
-	for (auto n : cartesianPoints) n.print();
-	std::cout << '\n';
-
-	for (size_t i = 0; i < polarPoints.size(); ++i) {
-		if (!polarPoints[i].isEqual(convertedPolarPoints[i])) {
-			correct = false;
-			break;
+	// Двовимірний простір: Декартова та полярна системи координат
+	{
+		for (const auto& polar : polarPoints) {
+			cartesianPoints2D.push_back(polarToCartesian(polar));
 		}
+
+		for (const auto& cartesian : cartesianPoints2D) {
+			convertedPolarPoints.push_back(cartesianToPolar(cartesian));
+		}
+
+		for (auto n : cartesianPoints2D) n.print2D();
+		std::cout << '\n';
+		for (auto n : convertedPolarPoints) n.print();
+		std::cout << '\n';
+
+		for (size_t i = 0; i < polarPoints.size(); ++i) {
+			if (!polarPoints[i].isEqual(convertedPolarPoints[i])) {
+				correct = false;
+				break;
+			}
+		}
+
+		if (correct) std::cout << "Перетворення виконані коректно.\n\n";
+		else std::cout << "Помилка в перетвореннях.\n\n";
 	}
 
-	if (correct) std::cout << "Перетворення виконані коректно.\n";
-	else std::cout << "Помилка в перетвореннях.\n";
+	// Тривимірний простір: Декартова та сферична системи координат
+	{
+		for (const auto& spherical : sphericalPoints) {
+			cartesianPoints3D.push_back(sphericalToCartesian(spherical));
+		}
+
+		for (const auto& cartesian : cartesianPoints3D) {
+			convertedSphericalPoints.push_back(cartesianToSpherical(cartesian));
+		}
+
+		for (auto n : cartesianPoints3D) n.print3D();
+		std::cout << '\n';
+		for (auto n : convertedSphericalPoints) n.print();
+		std::cout << '\n';
+
+		for (size_t i = 0; i < sphericalPoints.size(); ++i) {
+			if (!sphericalPoints[i].isEqual(convertedSphericalPoints[i])) {
+				correct = false;
+				break;
+			}
+		}
+
+		if (correct) std::cout << "Перетворення виконані коректно.\n";
+		else std::cout << "Помилка в перетвореннях.\n";
+	}
 
 	return 0;
 }
